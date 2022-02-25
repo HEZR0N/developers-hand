18fab23b06416cbd4c1e66cc32b97328d25d1317
HEZR0N <hezron.perez@my.utsa.edu>
2022-02-25 11:31:54 -0600

    Demo JavaFX project slightly modded

---
+++ DevelopersHand/src/application/SampleController.java
@@ -0,0 +1,17 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SampleController {
+
+    @FXML
+    private Button juniorDeck;
+
+    @FXML
+    private Button internDeck;
+
+    @FXML
+    private Button itemDeck;
+
+}
