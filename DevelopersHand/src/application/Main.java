fbb9575751ab9208fe28126c509fbce793b3fea1
HEZR0N <hezron.perez@my.utsa.edu>
2022-03-07 13:29:47 -0600

    added Logo (first attempt)

--- DevelopersHand/src/application/Main.java
+++ DevelopersHand/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -15,6 +16,8 @@ public class Main extends Application {
 			Scene scene = new Scene(root,800,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Developer's Hand");
+			Image logo = new Image("developers-hand-logo.png");
+			primaryStage.getIcons().add(logo);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
