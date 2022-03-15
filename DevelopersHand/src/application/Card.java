816ec1412c636589fd68c7d32980ba81b9246582
bagoferasers <ctrul@DESKTOP-3F4BB86>
2022-03-14 21:08:18 -0500

    Finished documentation of class.

--- DevelopersHand/src/application/Card.java
+++ DevelopersHand/src/application/Card.java
@@ -2,6 +2,7 @@
  * This class is a basic definition of a Card object. It contains 
  * a definition, constructor, getters, and setters.
  */
+
 package application;
 
 import javafx.scene.image.Image;
@@ -11,34 +12,68 @@ public class Card {
 	private String description;
 	private Image picture;
 	private String event;
+	
 	/**
-	 * This is a constructor for a basic Card object.
-	 * @param String 
+	 * Constructor for a basic Card object.
+	 * @param String n is the name of the Card.
 	 */
 	public Card(String n) {
 		this.setName(n);
 	}
+	
+	/**
+	 * @return the name of the Card object.
+	 */
 	public String getName() {
 		return this.name;
 	}
+	
+	/**
+	 * @param n is the name of the Card object.
+	 */
 	public void setName(String n) {
 		this.name = n;
 	}
+	
+	/**
+	 * @return the description of the Card object.
+	 */
 	public String getDescription() {
 		return this.description;
 	}
+	
+	/**
+	 * @param d is the description of the Card object.
+	 */
 	public void setDescription(String d) {
 		this.description = d;
 	}
+	
+	/**
+	 * @return the picture of the Card object.
+	 */
 	public Image getPicture() {
 		return this.picture;
 	}
+	
+	/**
+	 * @param p is the picture of the Card object.
+	 */
 	public void setPicture(Image p) {
 		this.picture = p;
 	}
+	
+	/**
+	 * 
+	 * @return the event of the Card object.
+	 */
 	public String getEvent() {
 		return this.event;
 	}
+	
+	/**
+	 * @param e is the event of the Card object.
+	 */
 	public void setEvent(String e) {
 		this.event = e;
 	}
