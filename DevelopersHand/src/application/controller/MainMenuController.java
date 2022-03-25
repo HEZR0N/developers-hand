b73683a2a6dd543c2eeee972ec89bc0d6070646f
Ahmet <1yigitbilici@gmail.com>
2022-03-25 11:13:05 -0500

    Added MainMenu.fxml and MainMenuController.java

---
+++ DevelopersHand/src/application/controller/MainMenuController.java
@@ -0,0 +1,42 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+public class MainMenuController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private Button buttonAbout;
+
+    @FXML
+    private Button buttonPlay;
+
+    @FXML
+    private Button buttonRules;
+
+    @FXML
+    void d74125(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goPlay(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goAbout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goRules(ActionEvent event) {
+
+    }
+
+}
