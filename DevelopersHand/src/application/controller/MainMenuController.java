f881d8e7461b702fa8855b6107df3ba98fe964aa
Ahmet <1yigitbilici@gmail.com>
2022-04-03 12:25:19 -0500

    Updated About and Rules scenes

--- DevelopersHand/src/application/controller/MainMenuController.java
+++ DevelopersHand/src/application/controller/MainMenuController.java
@@ -1,18 +1,19 @@
 package application.controller;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
-
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MainMenuController {
@@ -45,24 +46,18 @@ public class MainMenuController {
     }
 
     @FXML
-    void goAbout(ActionEvent event) {
-    	JFrame jFrame = new JFrame();
-    	jFrame.setTitle("About");
-    	JOptionPane.showMessageDialog(jFrame, "1.Press play\n"
-    					   + "2.Main Game screen appears\n"
-    					   + "3.Welcome message appears\n"
-    					   + "4.Round 1 begins\n"
-    					   + "5.Player draws a scenario card from intern deck\n"
-    					   + "6.Player reads description and accepts card\n"
-    					   + "   i.Objective appears in objective section\n"
-    					   + "  ii.If a player plays an action card that is an objective, 3x XP gets added\n"
-    					   + "7.Round 2 begins\n"
-    					   + "8.Player Draws 2 action cards, reads, accepts\n"
-    					   + "9.Round 3");
-//    	Alert alert = new Alert(AlertType.INFORMATION);
-//    	alert.setTitle("About");
-//    	alert.setHeaderText("Game Procedure");
-//    	alert.setContentText("1.Press play\n"
+    void goAbout(ActionEvent event) throws IOException {
+    	URL url = new File("src/About.fxml").toURI().toURL();
+    	Parent root = FXMLLoader.load(url);
+    	Stage stage = new Stage();
+    	Scene scene = new Scene(root);
+    	stage.setTitle("About");
+    	stage.setScene(scene);
+    	stage.initModality(Modality.APPLICATION_MODAL);
+    	stage.show();
+//    	JFrame jFrame = new JFrame();
+//    	jFrame.setTitle("About");
+//    	JOptionPane.showMessageDialog(jFrame, "1.Press play\n"
 //    					   + "2.Main Game screen appears\n"
 //    					   + "3.Welcome message appears\n"
 //    					   + "4.Round 1 begins\n"
@@ -73,51 +68,37 @@ public class MainMenuController {
 //    					   + "7.Round 2 begins\n"
 //    					   + "8.Player Draws 2 action cards, reads, accepts\n"
 //    					   + "9.Round 3");
-//    	alert.show();
     }
 
     @FXML
-    void goRules(ActionEvent event) {
-    	JFrame jFrame = new JFrame();
-    	jFrame.setTitle("Rules");
-    	JOptionPane.showMessageDialog(jFrame, "1.Each round a player can either\n"
-				+ "	  i.Draw a scenario card and get an objective (Card added to hand)\n"
-				+ "		a.You can only ever have one objective. Drawn another, and current objective is overwritten.\n"
-				+ "	 ii.Draw 2 action cards\n"
-				+ " iii.Draw 1 action card and play 1 action card\n"
-				+ "  iv.Play 2 action cards\n"
-				+ "2.When playing an action card, what can happen\n"
-				+ "   i.Skill level up (Gain XP, possible objective)\n"
-				+ "  ii.Code merged (Gain XP, possible objective)\n"
-				+ " iii.Bug introduced (Lose Current Objective, Gain one bug)\n"
-				+ "  iv.To resolve a bug, player selects card, clicks resolve, and loses one action card (Gains XP, possible objective)\n"
-				+ "3.Lose Condition: have 3 unresolved bugs (Shows poor time management and lack of skill)\n"
-				+ "4.Win Condition: Gain 123 XP: Get promoted from intern to developer!\n"
-				+ "5.Strategy\n"
-				+ "   i.May want to hold on to action cards until a certain skill higher (player.XP+=card.XP+player.getSkillLevel(cardSkill)*2)\n"
-				+ "  ii.May want to draw a different scenario/objective\n"
-				+ " iii.May want to resolve bug");
-//    	Alert alert = new Alert(AlertType.INFORMATION);
-//    	alert.setTitle("Rules");
-//    	alert.setHeaderText("Rules");
-//    	alert.setContentText("1.Each round a player can either\n"
-//    						+ "	  i.Draw a scenario card and get an objective (Card added to hand)\n"
-//    						+ "		a.You can only ever have one objective. Drawn another, and current objective is overwritten.\n"
-//    						+ "	 ii.Draw 2 action cards\n"
-//    						+ " iii.Draw 1 action card and play 1 action card\n"
-//    						+ "  iv.Play 2 action cards\n"
-//    						+ "2.When playing an action card, what can happen\n"
-//    						+ "   i.Skill level up (Gain XP, possible objective)\n"
-//    						+ "  ii.Code merged (Gain XP, possible objective)\n"
-//    						+ " iii.Bug introduced (Lose Current Objective, Gain one bug)\n"
-//    						+ "  iv.To resolve a bug, player selects card, clicks resolve, and loses one action card (Gains XP, possible objective)\n"
-//    						+ "3.Lose Condition: have 3 unresolved bugs (Shows poor time management and lack of skill)\n"
-//    						+ "4.Win Condition: Gain 123 XP: Get promoted from intern to developer!\n"
-//    						+ "5.Strategy\n"
-//    						+ "   i.May want to hold on to action cards until a certain skill higher (player.XP+=card.XP+player.getSkillLevel(cardSkill)*2)\n"
-//    						+ "  ii.May want to draw a different scenario/objective\n"
-//    						+ " iii.May want to resolve bug");
-//    	alert.show();
+    void goRules(ActionEvent event) throws IOException {
+    	URL url = new File("src/Rules.fxml").toURI().toURL();
+    	Parent root = FXMLLoader.load(url);
+    	Stage stage = new Stage();
+    	Scene scene = new Scene(root);
+    	stage.setTitle("About");
+    	stage.setScene(scene);
+    	stage.initModality(Modality.APPLICATION_MODAL);
+    	stage.show();
+//    	JFrame jFrame = new JFrame();
+//    	jFrame.setTitle("Rules");
+//    	JOptionPane.showMessageDialog(jFrame, "1.Each round a player can either\n"
+//				+ "	  i.Draw a scenario card and get an objective (Card added to hand)\n"
+//				+ "		a.You can only ever have one objective. Drawn another, and current objective is overwritten.\n"
+//				+ "	 ii.Draw 2 action cards\n"
+//				+ " iii.Draw 1 action card and play 1 action card\n"
+//				+ "  iv.Play 2 action cards\n"
+//				+ "2.When playing an action card, what can happen\n"
+//				+ "   i.Skill level up (Gain XP, possible objective)\n"
+//				+ "  ii.Code merged (Gain XP, possible objective)\n"
+//				+ " iii.Bug introduced (Lose Current Objective, Gain one bug)\n"
+//				+ "  iv.To resolve a bug, player selects card, clicks resolve, and loses one action card (Gains XP, possible objective)\n"
+//				+ "3.Lose Condition: have 3 unresolved bugs (Shows poor time management and lack of skill)\n"
+//				+ "4.Win Condition: Gain 123 XP: Get promoted from intern to developer!\n"
+//				+ "5.Strategy\n"
+//				+ "   i.May want to hold on to action cards until a certain skill higher (player.XP+=card.XP+player.getSkillLevel(cardSkill)*2)\n"
+//				+ "  ii.May want to draw a different scenario/objective\n"
+//				+ " iii.May want to resolve bug");
     }
 
 }
